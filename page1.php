7d8c6ad60f1d5a568307875cc66d0e56ba596a7e
OpenShift guest <seattle.and.sydney@gmail.com>
2013-01-23 20:49:56 -0500

    From Cloud9

---
+++ page1.php
@@ -0,0 +1,13 @@
+<body>
+
+    <?php    
+        include("navMenu.php");    
+    ?>
+
+    <form action="Page2.php" method="post">
+        Name: <input type="text" name="fname">
+        Age: <input type="text" name="age">
+        <input type="submit">
+    </form>
+    
+</body>
